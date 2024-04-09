6be3b6824d5b0db977ed577fa6439531b0f3c0c9
alex_fflab <alexandra.ludwig@medizin.uni-leipzig.de>
2024-04-09 10:32:22 +0200

    1

---
+++ select_random_masker.py
@@ -0,0 +1,22 @@
+
+
+import random
+import subprocess
+
+from glob import glob
+
+masker_files = (glob('olkisa_masker/*.wav'))
+
+count: int = 0
+max_count: int = 5
+
+while count < max_count:
+    for i in range(len(masker_files)):
+        random_masker = (random.choice(masker_files))
+        return_code = subprocess.call(["afplay", random_masker])
+        break
+    count = count + 1
+    if count == max_count:
+        print('Test zu Ende')
+
+
