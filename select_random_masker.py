ac27b1e58df4ac533bb189b3f495f65099496ec1
alex_fflab <alexandra.ludwig@medizin.uni-leipzig.de>
2024-04-23 17:08:09 +0200

    23.4. random targets

--- select_random_masker.py
+++ select_random_masker.py
@@ -14,11 +14,18 @@ def select_random_speech(target_path=Path.cwd() / 'olkisa_targets',
     target_wavs = []
     wav_list = []
 
-    for i in range(n_trials):
-        print(i)
-        random_masker_path = (random.choice(masker_files))
-        random_masker_wav = slab.Sound(random_masker_path)
-        masker_wavs.append(random_masker_wav)
+    while n_trials <= 30:
+        for i in range(n_trials):
+            print(i)
+            random_masker_path = (random.choice(masker_files))
+            random_masker_wav = slab.Sound(random_masker_path)
+            masker_wavs.append(random_masker_wav)
+        for i in range(n_trials):
+            print(i)
+            random_target_path = (random.choice(target_files))
+            random_target_wav = slab.Sound(random_target_path)
+            target_wavs.append(random_target_wav)
+
 
         # todo do this with target sounds
 
