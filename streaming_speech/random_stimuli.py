ddc3e81f865cae88b3e4a9e1470f09728a3b5504
alex_fflab <alexandra.ludwig@medizin.uni-leipzig.de>
2024-08-20 17:31:58 +0200

    work on noise streaming

--- streaming_speech/random_stimuli.py
+++ streaming_speech/random_stimuli.py
@@ -6,7 +6,6 @@ from pathlib import Path
 import numpy
 import slab
 
-# https://nitratine.net/blog/post/simulate-keypresses-in-python/#installing-pynput
 
 def select_random_speech(n_trials=30, level=85, wav_path=Path.cwd() / 'data' / 'wav_data'):
 
