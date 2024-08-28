84a828f5f26b8d22cd1d5f39f61a3bab38bfc102
pfriedrich-hub <lukas.lange01@gmail.com>
2024-08-28 16:38:18 +0200

    connect game to streaming noise

--- streaming_noise/game/python_for_jar.py
+++ streaming_noise/game/python_for_jar.py
@@ -1,41 +1,16 @@
-
-#!/usr/bin/env python
-import argparse
-import logging
-import os
-import re
-import shlex
+from pathlib import Path
 import signal
 import sys
 import subprocess
 
-log = logging.getLogger(__name__)
-SCRIPT = os.path.dirname(os.path.realpath(__file__))
-JAR_LOCATION = SCRIPT + "/spaceship.jar"  # TODO based on name and relative path to jar file
-DEFAULT_JAVA_OPTS = ""  # TODO fill this with any defaults?
+JAR_LOCATION = Path.cwd() / 'streaming_noise' / 'game' / 'spaceship.jar'  # TODO based on name and relative path to jar file
 
 # + richtig, - falsch, + Game over, ESC Reset
 
-def arg_parser():
-    p = argparse.ArgumentParser(add_help=False)
-    p.add_argument('--java-options', default=DEFAULT_JAVA_OPTS)
-    p.add_argument('--jar-path', default=JAR_LOCATION)
-    return p
-
-
-def main(args=None):
-    p = arg_parser()
-    args, remainder = p.parse_known_args(args)
-
-    java_options = list(DEFAULT_JAVA_OPTS)
-    if args.java_options:
-        java_options.extend(shlex.split(args.java_options))
-
-    cmd = ['java'] + java_options + remainder + ['-jar', JAR_LOCATION]
-    log.critical("Wrapper launching: {}".format(cmd))
-
+def main():
+    cmd = ['java', '-jar', JAR_LOCATION]
+    print(cmd)
     p = subprocess.Popen(cmd)
-
     try:
         p.wait()
     except KeyboardInterrupt:
@@ -44,7 +19,5 @@ def main(args=None):
     except subprocess.CalledProcessError as e:
         sys.exit(e.returncode)
 
-
 if __name__ == "__main__":
-    logging.basicConfig()
     main()
--- streaming_noise/streaming_noise.py
+++ streaming_noise/streaming_noise.py
@@ -6,6 +6,9 @@ import time
 from pathlib import Path
 data_path = Path.cwd() / 'data'
 from matplotlib import pyplot as plt
+from pynput.keyboard import Controller
+keyboard = Controller()
+# import game.python_for_jar as python_for_jar
 
 def noise_streaming(masker_on=True):
 
@@ -28,8 +31,8 @@ def noise_streaming(masker_on=True):
             n_stim = 160
             n_targets = 18
         else:
-            n_stim = 60
-            n_targets = 8
+            n_stim = 100
+            n_targets = 12
 
         # timing parameters
         soa = 550  # time between onsets (stim onset async)
@@ -49,7 +52,7 @@ def noise_streaming(masker_on=True):
         target_amp_list = []
         for i in range(n_targets):
             list = [non_target_amp] * n_list
-            rnd_idx = numpy.random.randint(n_list / 4, n_list - n_list / 4)  # leave the first and last places free
+            rnd_idx = numpy.random.randint(numpy.round(n_list / 4), n_list - numpy.round(n_list / 4))  # leave the first and last places free
             list[rnd_idx] = target_amp
             # print(list)
             target_amp_list.extend(list)
@@ -72,33 +75,54 @@ def noise_streaming(masker_on=True):
         freefield.write(tag='target_channel', value=freefield.pick_speakers(target_speaker_id)[0].analog_channel,
                         processors=freefield.pick_speakers(target_speaker_id)[0].analog_proc)
 
+        # reset masker channels before reassigning them to a new condition
+        freefield.write(tag='masker_l_channel', value=99, processors=['RX81', 'RX82'])
+        freefield.write(tag='masker_r_channel', value=99, processors=['RX81', 'RX82'])
+
         if masker_on:
             # write masker parameters
             # left
             freefield.write(tag='masker_l_amps', value=masker_l_amp_list,
-                            processors=freefield.pick_speakers(masker_l_speaker_id)[0].analog_proc)
+                            processors=['RX81', 'RX82'])
             freefield.write(tag='masker_l_channel', value=freefield.pick_speakers(masker_l_speaker_id)[0].analog_channel,
                             processors=freefield.pick_speakers(masker_l_speaker_id)[0].analog_proc)
-
             # right
             freefield.write(tag='masker_r_amps', value=masker_r_amp_list,
-                            processors=freefield.pick_speakers(masker_r_speaker_id)[0].analog_proc)
+                            processors=['RX81', 'RX82'])
             freefield.write(tag='masker_r_channel', value=freefield.pick_speakers(masker_r_speaker_id)[0].analog_channel,
                             processors=freefield.pick_speakers(masker_r_speaker_id)[0].analog_proc)
-        input(f'Next condition {} Press button to start.')
+
+        input(f'Next condition {(-azimuth, azimuth)} Press button to start.')
+        freefield.wait_for_button('RX82', 'button')
+
         start_time = time.time()
         freefield.play()
-        # freefield.wait_to_finish_playing(proc='all', tag='')
+        n_hits = 0
+        n_missed = 0
+        n_false_positives = 0
         while start_time + (n_stim * soa / 1000) > time.time():
-        # todo check whether hits or misses or false positives change and communicate to game
+            time.sleep(0.1)
+            hits = freefield.read('n_hits', 'RX82')
+            missed = freefield.read('n_missed', 'RX82')
+            false_positives = freefield.read('n_fp', 'RX82')
+            if hits == n_hits + 1:
+                press('+')
+                print('hit')
+                n_hits = hits
+            if missed == n_missed + 1:
+                press('-')
+                print('miss')
+                n_missed = missed
+            if false_positives == n_false_positives + 1:
+                press('-')
+                print('fp')
+                n_false_positives = false_positives     # todo check fp
 
+                # todo write to excel
 
-        hits = freefield.read('n_hits', 'RX82')
-        missed = freefield.read('n_missed', 'RX82')
-        false_positives = freefield.read('n_fp', 'RX82')
-        n_buttons = freefield.read('n_buttons', 'RX82')
-
-        # todo write to xls
+def press(key):
+    keyboard.press(key)
+    keyboard.release(key)
 
 if __name__ == "__main__":
     # initialize processors
@@ -106,8 +130,7 @@ if __name__ == "__main__":
                  ['RX82', 'RX8', data_path / 'rcx' / 'streaming_noise.rcx'],
                  ['RP2', 'RP2', data_path / 'rcx' / '4_buttons.rcx']]
     freefield.initialize(setup='dome', device=proc_list)
-    freefield.load_equalization(data_path / 'calibration' / 'calibration_dome_01.03.pkl')
     freefield.set_logger('info')
     noise_streaming(masker_on=False)
-    noise_streaming()
+    # noise_streaming(masker_on=True)
     freefield.halt()
